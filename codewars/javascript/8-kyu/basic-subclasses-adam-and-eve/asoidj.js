b9478cf3ccc99a5fc348901026d9f05dc31b86ce
Mikael Carvalho Mendes <111322765+MikaelCM@users.noreply.github.com>
2025-08-27 05:05:42 -0300

    Add files via upload

---
+++ codewars/javascript/8-kyu/basic-subclasses-adam-and-eve/asoidj.js
@@ -0,0 +1,15 @@
+// Link: https://www.codewars.com/kata/547274e24481cfc469000416
+
+class Human {}
+
+class Man extends Human {}
+
+class Woman extends Human {}
+
+class God {
+    static create() {
+        return [new Man(), new Woman()]
+    }
+}
+
+const [adam, eve] = God.create()
